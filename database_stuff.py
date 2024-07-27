774a996bb1d359587f263a20165bf92e092241da
AlanMainstriker <hgames14@mail.ru>
2024-07-27 12:32:47 +0300

    Общая БД для всех пользователей

--- database_stuff.py
+++ database_stuff.py
@@ -2,8 +2,7 @@ from typing import Optional
 from sqlmodel import Field, SQLModel, create_engine
 
 
-sqlite_file_name = "tasks.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_url = 'mysql://sql7722457:HRt9kU4N16@sql7.freemysqlhosting.net:3306/sql7722457'
 engine = create_engine(sqlite_url, echo=True, pool_size=100, max_overflow=0)
 
 
